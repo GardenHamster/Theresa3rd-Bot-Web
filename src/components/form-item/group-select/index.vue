6201e63c12dbbc5eb827e726e908764ce18ce354
GardenHamster <zqn260256@gmail.com>
2023-10-14 22:46:14 +0800

    Update index.vue

--- src/components/form-item/group-select/index.vue
+++ src/components/form-item/group-select/index.vue
@@ -1,14 +1,15 @@
 <template>
-    <a-select v-model:model-value="modelValue" :options="groupOptions" :style="{ minHeight: '100px' }" :scrollbar="true" @change="onChange" allow-search allow-clear multiple>
+    <a-select v-model:model-value="modelValue" :options="groupOptions" :style="{ minHeight: '100px' }" :scrollbar="true"
+        @change="onChange" allow-search allow-clear multiple>
         <a-option :value="0">所有群</a-option>
     </a-select>
 </template>
 
 <script lang="ts" setup>
-import { ref, computed, watch } from 'vue';
+import { computed } from 'vue';
 import type { SelectOptionData } from '@arco-design/web-vue/es/select/interface';
 
-let fullOptions: SelectOptionData[] = [];
+let initValues: number[] = [];
 const props = withDefaults(defineProps<{ modelValue?: number[], options: SelectOptionData[], placeholder?: string }>(), { placeholder: '选择一个或多个群' })
 const emit = defineEmits<{ (e: "update:modelValue", value: number[]): void }>();
 const modelValue = computed({
@@ -32,19 +33,22 @@ const disableAll = (options: SelectOptionData[]): SelectOptionData[] => {
     return options;
 }
 
+const getInitValues = (): number[] => {
+    if (initValues.length > 0) return initValues;
+    initValues = props.modelValue ?? [];
+    return initValues;
+}
+
 const getFullOptions = (): SelectOptionData[] => {
-    console.log('props.modelValue', props.modelValue);
-    if (fullOptions.length > 0) return fullOptions;
-    const values = props.modelValue ?? [];
+    const values = getInitValues();
     const options: SelectOptionData[] = Array.from(props.options, o => { return { ...o } });
     for (let index = 0; index < values.length; index += 1) {
         const groupId = values[index];
         if (groupId === 0) continue;
         if (options.some(o => o.value === groupId)) continue;
-        const newOption: SelectOptionData = { label: `???(${groupId})`, value: groupId };
+        const newOption: SelectOptionData = { label: `${groupId}(${groupId})`, value: groupId };
         options.push(newOption);
     }
-    fullOptions = options;
     return options;
 }
 
@@ -56,13 +60,8 @@ const onChange = (value: unknown) => {
 
 const groupOptions = computed(() => {
     const options = getFullOptions();
-    console.log('options', options);
     return modelValue.value.some(o => o === 0) ? activeAll(options) : disableAll(options);
 })
-
-watch(modelValue, async (newValue) => {
-    // console.log('newValue', newValue);
-});
 
 
 </script>
